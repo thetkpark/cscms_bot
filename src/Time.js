10231d230bdfd4a5b64f788094c424d6438994e5
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2019-09-20 21:58:28 +0700

    add date showing option

--- src/Time.js
+++ src/Time.js
@@ -1,15 +1,20 @@
 function convertTime(str) {
     //const data = (\d{4})-(\d{2}-(\d{2}) (\d{2}):(\d{2}):(\d{2})).exec()
-    console.log(`str: ${str}`);
     const date = new Date(str)
-    console.log(`date: ${date}`);
     const time = date.toLocaleDateString('en-US-u-ca-gregory', {
         dateStyle: 'medium',
         timeZone: 'Asia/Bangkok',
         timeStyle: 'medium',
-        hour12: false
+        hour12: false,
+        weekday: 'short', 
+        year: 'numeric', 
+        month: 'short', 
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZoneName: 'short'
     })
-    console.log(`time: ${time}`);
     return time
 }
 
