2e6a1ef4e03af338c4e2b074f7dba35cccac0057
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2019-09-20 16:01:09 +0700

    add debug

--- src/Time.js
+++ src/Time.js
@@ -1,12 +1,15 @@
 function convertTime(str) {
     //const data = (\d{4})-(\d{2}-(\d{2}) (\d{2}):(\d{2}):(\d{2})).exec()
+    console.log(`str: ${str}`);
     const date = new Date(str)
+    console.log(`date: ${date}`);
     const time = date.toLocaleDateString('en-US-u-ca-gregory', {
         dateStyle: 'medium',
         timeZone: 'Asia/Bangkok',
         timeStyle: 'medium',
         hour12: false
     })
+    console.log(`time: ${time}`);
     return time
 }
 
