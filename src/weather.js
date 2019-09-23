a91638f5a1f1981fce88711608c7b2dc90a2d456
Sethanant Pipatpakorn <the_tkpark@hotmail.com>
2019-09-23 21:09:48 +0700

    remove debug clog

--- src/weather.js
+++ src/weather.js
@@ -4,7 +4,6 @@ require('dotenv').config()
 async function getCurrentWeather(){
     const url = `http://dataservice.accuweather.com/currentconditions/v1/3558717?apikey=${process.env.WEATHER_API_KEY}`
     const {data} = await axios.get(url)
-    console.log(data);
     const temp = data[0].Temperature.Metric
     return `Time: ${data[0].LocalObservationDateTime}\n🌡 Temperatur: ${temp.Value}℃\n💦 Precipitation: ${data[0].HasPrecipitation}\n⛅️ Description: ${data[0].WeatherText}`
     
