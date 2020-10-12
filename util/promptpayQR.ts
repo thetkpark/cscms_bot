e74dab5a8ef0b45483f805e23454bd6cdfef145e
Kavisara Srisuwatcharee <kavisara2000@gmail.com>
2020-10-12 14:11:01 +0700

    [Refine] getQR payment

--- util/promptpayQR.ts
+++ util/promptpayQR.ts
@@ -1,21 +1,21 @@
-const qrcode = require('qrcode')
+import qrcode, { QRCodeToDataURLOptions } from "qrcode";
 const generatePayload = require('promptpay-qr')
 const imgur = require('imgur')
 
-export async function generatePromptPayQR(promptPayID: string, amount: number) {
+export async function generatePromptPayQR(promptPayID: string, amount: number): Promise<string> {
 	const payload = generatePayload(promptPayID, { amount })
 	
-	const options = { type: 'png', color: { dark: '#003b6a', light: '#f7f8f7' } }
+	const options: QRCodeToDataURLOptions = { type: 'image/png' , color: { dark: '#003b6a', light: '#f7f8f7' }}
 
 	try {
-		const png = await qrcode.toDataURL(payload, options)
+		const png: string = await qrcode.toDataURL(payload, options)
 		return png
 	} catch (err) {
 		return err
 	}
 }
 
-export async function getPromptPayQR(promptPayID: string, amount: string) {
+export async function getPromptPayQR(promptPayID: string, amount: string): Promise<string> {
 	try {
 		const png = await generatePromptPayQR(promptPayID, parseFloat(amount))
 		let payload = png.split(',')
